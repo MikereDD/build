WhyProject/android_build

Change how we search for custom makefiles
Change-Id: I00fdb8f425f675bff23924620bedb7d1c6a1b987

core/product_config.mk
@@ -181,8 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(CUSTOM_BUILD),)
-    $(call import-products, device/why/$(CUSTOM_BUILD)/device.mk)
-    all_product_configs := device/why/$(CUSTOM_BUILD)/device.mk
+    all_product_configs := device/*/$(CUSTOM_BUILD)/why.mk
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.

MikereDD/build

core/product_config.mk
@@ -181,8 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(AQUA_BUILD),)
-    $(call import-products, device/aquarios/$(AQUA_BUILD)/device.mk)
-    all_product_configs := device/aquarios/$(AQUA_BUILD)/device.mk
+    all_product_configs := device/*/$(AQUA_BUILD)/aquarios.mk
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.