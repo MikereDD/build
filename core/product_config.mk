WhyProject/android_build

Fix fastpath code for custom builds
Change-Id: I3207dc1c8870548bc6536a9a401b712bce706c4e

core/product_config.mk
@@ -181,7 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(CUSTOM_BUILD),)
-    all_product_configs := device/*/$(CUSTOM_BUILD)/why.mk
+    all_product_configs := $(shell ls device/*/$(CUSTOM_BUILD)/why.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.

MikereDD/build

core/product_config.mk
@@ -181,7 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(AQUA_BUILD),)
-    all_product_configs := device/*/$(AQUA_BUILD)/aquarios.mk
+    all_product_configs := $(shell ls device/*/$(AQUA_BUILD)/aquarios.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.